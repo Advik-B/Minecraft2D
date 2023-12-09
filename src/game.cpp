d1e715c5740e02748d380bc589e7ff650b1fca78
Advik <advik.b@gmail.com>
2023-12-09 23:28:29 +0530

    Update game.cpp

--- src/game.cpp
+++ src/game.cpp
@@ -1,10 +1,14 @@
 #include <SFML/Graphics.hpp>
+#include <vector>
+#include <iostream>
 #include "colour_utils.cpp"
 #include "gameclasses/block.cpp"
 
 class Game {
     public:
         Game();
+        ~Game();
+
         int run();
         void onExit();
         void beforeStart();
@@ -23,6 +27,9 @@ class Game {
         void render();
         void keyPress(sf::Event::KeyEvent event);
         void keyRelease(sf::Event::KeyEvent event);
+
+        std::vector<Block> blocks;
+        sf::Sprite sprite;
 };
 
 Game::Game() {
@@ -34,6 +41,32 @@ Game::Game() {
     );
     window.setVerticalSyncEnabled(true);
     backgroundColour = rgb(31, 32, 40);
+
+    TextureLoader::TextureMap textureMap = TextureLoader::loadTextures();
+    Block block = Block();
+    block.setTexture("dirt", textureMap);
+    block.setPos(sf::Vector2f(100, 100));
+    blocks.push_back(block);
+    for (auto block : blocks) {
+//        block.texture.load();
+    }
+    sf::Texture texture;
+    texture.loadFromFile("stone.png");
+
+    // Resize the sprite to 512x512 pixels
+    texture.setSmooth(true);
+    texture.setRepeated(true);
+
+    sf::Sprite sprite;
+//    sprite.setScale(10, 10);
+    sprite.setTexture(texture);
+    sprite.setPosition(100, 100);
+    this->sprite = sprite;
+
+}
+
+Game::~Game() {
+    window.close();
 }
 
 int Game::run() {
@@ -76,6 +109,11 @@ void Game::update() {
 
 void Game::render() {
     window.clear(backgroundColour);
+    for (auto block : blocks) {
+        block.drawTo(&window);
+    }
+    window.draw(sprite);
+
     window.display();
 }
 
