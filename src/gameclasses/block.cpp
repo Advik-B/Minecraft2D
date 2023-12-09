7d4b41ab2726c35801c214aecf312e0c6b89b656
Advik <advik.b@gmail.com>
2023-12-09 22:17:34 +0530

    Update block.cpp

--- src/gameclasses/block.cpp
+++ src/gameclasses/block.cpp
@@ -1,17 +1,54 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/RenderTarget.hpp>
 #include <string>
 #include <map>
 #include "texture.cpp"
+
+struct BlockAttributes {
+    int hardness;
+    int resistance; // to explosions
+    int emmisivity; // light level
+
+};
 
 
 class Block {
     public:
         Block();
         void setTexture(std::string texture, std::map<std::string, Texture> textureMap);
+        void drawTo(sf::RenderWindow target);
+        void setAttributes(BlockAttributes attributes);
+        void setPos(sf::Vector2f pos);
+        void move(sf::Vector2f offset);
+        Texture texture;
 
     private:
         sf::Sprite sprite;
-        Texture texture;
-
-
+        BlockAttributes attributes;
 };
+
+Block::Block() {
+    this->sprite = sf::Sprite();
+}
+
+void Block::setTexture(std::string texture, std::map<std::string, Texture> textureMap) {
+    this->texture = textureMap[texture];
+    this->sprite.setTexture(this->texture.getTexture());
+}
+
+void Block::drawTo(sf::RenderWindow target) {
+    target.draw(this->sprite);
+}
+
+void Block::setAttributes(BlockAttributes attributes) {
+    this->attributes = attributes;
+}
+
+void Block::setPos(sf::Vector2f pos) {
+    this->sprite.setPosition(pos);
+}
+
+void Block::move(sf::Vector2f offset) {
+    this->sprite.move(offset);
+}
+
