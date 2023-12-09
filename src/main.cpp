faf80d6080ece3c5f7d95ebad1f29630cfe34e48
Advik <advik.b@gmail.com>
2023-12-09 15:37:39 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,6 @@
-#include <SFML/Graphics.hpp>
+#include "game.cpp"
 
-class Game {
-    public:
-        Game();
-        void run();
-        void onExit() {};
-        void beforeStart() {};
-    private:
-        sf::RenderWindow window;
-        void processEvents();
-        void update();
-        void render();
-};
-
-Game::Game() {
-    sf::RenderWindow window(
-            sf::VideoMode::getDesktopMode(),
-            "SFML works!",
-            sf::Style::Fullscreen,
-            sf::ContextSettings(24, 8, 4)
-    );
+int main() {
+    auto game = Game();
+    return game.run();
 }
-
-void Game::run() {
-    while (window.isOpen()) {
-        processEvents();
-        update();
-        render();
-    }
-}
-
-
