bd2f8e9776316bca5020a0902ab3f231df380111
Advik <advik.b@gmail.com>
2023-12-09 21:39:26 +0530

    Update texture.cpp

--- src/gameclasses/texture.cpp
+++ src/gameclasses/texture.cpp
@@ -36,8 +36,52 @@ std::map<std::string, Texture> loadTextures() {
     std::string paintingPath = TEXTURE_PATH "painting/";
     std::string titlescreenPath = TEXTURE_PATH "titlescreen/";
 
+    std::map<std::string, Texture> textures;
+    // textures["block.dirt"] = Texture(blockPath + "dirt.png", TextureType::Opaque);
 
+    for (const auto & entry : fs::directory_iterator(blockPath)) {
+        std::string path = entry.path();
+        std::string name = path.substr(path.find_last_of("/") + 1);
+        textures["block." + name] = Texture(path, TextureType::Opaque);
+    }
 
+    for (const auto & entry : fs::directory_iterator(containerPath)) {
+        std::string path = entry.path();
+        std::string name = path.substr(path.find_last_of("/") + 1);
+        textures["container." + name] = Texture(path, TextureType::Opaque);
+    }
+
+    for (const auto & entry : fs::directory_iterator(itemPath)) {
+        std::string path = entry.path();
+        std::string name = path.substr(path.find_last_of("/") + 1);
+        textures["item." + name] = Texture(path, TextureType::Opaque);
+    }
+
+    for (const auto & entry : fs::directory_iterator(miscPath)) {
+        std::string path = entry.path();
+        std::string name = path.substr(path.find_last_of("/") + 1);
+        textures["misc." + name] = Texture(path, TextureType::Opaque);
+    }
+
+    for (const auto & entry : fs::directory_iterator(mobEffectPath)) {
+        std::string path = entry.path();
+        std::string name = path.substr(path.find_last_of("/") + 1);
+        textures["mob_effect." + name] = Texture(path, TextureType::Opaque);
+    }
+
+    for (const auto & entry : fs::directory_iterator(paintingPath)) {
+        std::string path = entry.path();
+        std::string name = path.substr(path.find_last_of("/") + 1);
+        textures["painting." + name] = Texture(path, TextureType::Opaque);
+    }
+
+    for (const auto & entry : fs::directory_iterator(titlescreenPath)) {
+        std::string path = entry.path();
+        std::string name = path.substr(path.find_last_of("/") + 1);
+        textures["titlescreen." + name] = Texture(path, TextureType::Opaque);
+    }
+
+    return textures;
 }
 
 class Texture
