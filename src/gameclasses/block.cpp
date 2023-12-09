2b7573ee02b7404fc8d1c3e2331132a023fc40b9
Advik <advik.b@gmail.com>
2023-12-09 22:03:40 +0530

    Update block.cpp

--- src/gameclasses/block.cpp
+++ src/gameclasses/block.cpp
@@ -1,3 +1,17 @@
 #include <SFML/Graphics.hpp>
+#include <string>
+#include <map>
 #include "texture.cpp"
 
+
+class Block {
+    public:
+        Block();
+        void setTexture(std::string texture, std::map<std::string, Texture> textureMap);
+
+    private:
+        sf::Sprite sprite;
+        Texture texture;
+        
+
+};
