5f48cd389ad2e2a7b59e3c9155396d063385bc80
Advik <advik.b@gmail.com>
2023-12-09 21:35:32 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 #include "game.cpp"
+#include <iostream>
+
+void Game::onExit() {}
+void Game::beforeStart() {}
+void Game::tick() {
+    std::cout << "tick" << std::endl;
+}
 
 int main() {
     auto game = Game();
+
     return game.run();
 }
