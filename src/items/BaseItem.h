95f9cfd43c9667194f7b1495f25ad86d6657d18a
Advik <advik.b@gmail.com>
2023-12-10 21:15:35 +0530

    Update BaseItem.h

--- src/items/BaseItem.h
+++ src/items/BaseItem.h
@@ -6,6 +6,8 @@
 #define MINECRAFT2D_BASEITEM_H
 
 #include <string>
+#include <SFML/Graphics/Sprite.hpp>
+#include "ItemContext.h"
 
 namespace minecraft {
     using namespace std;
@@ -20,7 +22,31 @@ namespace minecraft {
         public:
             BaseItem() = default;
             virtual ~BaseItem() = default;
+            BaseItem(const string &itemID);
 
+            virtual ItemType getItemType() const = 0;
+            virtual string getItemName() const = 0;
+            virtual string getItemDescription() const = 0;
+            virtual const sf::Sprite &getItemSprite() const = 0;
+            virtual const sf::Sprite &getCursorSprite() const = 0;
+            virtual const string getItemID() const = 0;
+            virtual void onEquip(ItemContext &context) = 0;
+            virtual void onUnequip(ItemContext &context) = 0;
+            virtual void primaryUse(ItemContext &context) = 0;
+            virtual void secondaryUse(ItemContext &context) = 0;
+            virtual void onTick(ItemContext &context) = 0;
+            virtual void onPickup(ItemContext &context) = 0;
+            virtual void onDrop(ItemContext &context) = 0;
+            virtual void destroy(ItemContext &context) = 0;
+
+        protected:
+            virtual void setItemSprite(const sf::Sprite &sprite) = 0;
+            virtual void setCursorSprite(const sf::Sprite &sprite) = 0;
+
+        private:
+            sf::Sprite cursorSprite; // Will be used for cursor when item is selected
+            sf::Sprite itemSprite; // Will be used for inventory and hotbar
+            const string itemID; // Unique ID for each item (used for serialization)
         };
 
     } // items
