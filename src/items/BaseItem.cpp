4f1ebc5bfe2a9f23a0710e13dcfe1ecf5a7ac0a2
Advik <advik.b@gmail.com>
2023-12-10 21:15:20 +0530

    Update BaseItem.cpp

--- src/items/BaseItem.cpp
+++ src/items/BaseItem.cpp
@@ -6,5 +6,28 @@
 
 namespace minecraft {
     namespace items {
+        BaseItem::BaseItem(const string &itemID) : itemID(itemID) {
+
+        }
+
+        const string &BaseItem::getItemID() const {
+            return itemID;
+        }
+
+        const sf::Sprite &BaseItem::getItemSprite() const {
+            return itemSprite;
+        }
+
+        const sf::Sprite &BaseItem::getCursorSprite() const {
+            return cursorSprite;
+        }
+
+        void BaseItem::setItemSprite(const sf::Sprite &sprite) {
+            itemSprite = sprite;
+        }
+
+        void BaseItem::setCursorSprite(const sf::Sprite &sprite) {
+            cursorSprite = sprite;
+        }
     } // items
 } // minecraft
