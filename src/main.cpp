51e3eef9a7e901168dcfdbe0d851736e76e7a97d
Advik <advik.b@gmail.com>
2024-01-28 22:58:24 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,45 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
 
-int main (int argc, char *argv[])
+#ifdef _WIN32
+#include <Windows.h>
+void debug(const char *format, ...)
+{
+    // Print a message to a console window, if not already present, create one.
+    static bool console_created = false;
+    if (!console_created)
+    {
+        AllocConsole();
+        freopen_s((FILE**)stdout, "CONOUT$", "w", stdout);
+        console_created = true;
+    }
+
+    // Print the message to the console window.
+    va_list args;
+    va_start(args, format);
+    vprintf(format, args);
+    va_end(args);
+}
+#else
+#include <iostream>
+void debug(const char *format, ...) {
+    // Print a message to the console.
+    va_list args;
+    va_start(args, format);
+    vprintf(format, args);
+    va_end(args);
+}
+#endif
+
+
+int main(int argc, char *argv[])
 {
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
-
+    debug("Arguments: %d\n", argc);
+    for (int i = 0; i < argc; i++)
+    {
+        debug("Argument %d: %s\n", i+1, argv[i]);
+    }
     while (window.isOpen())
     {
         sf::Event event;
